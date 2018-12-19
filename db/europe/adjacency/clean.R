bdd2670638f7a5612f8f96d3e7bf09bdd5e04ba1
swo <swo@alum.mit.edu>
2018-12-19 13:41:33 -0500

    europe adjacency, only europe

--- db/europe/adjacency/clean.R
+++ db/europe/adjacency/clean.R
@@ -11,19 +11,23 @@ pairs = read_csv('DirectContiguity320/contdird.csv') %>%
   select(code1 = state1no, code2 = state2no, year, contiguity_type = conttype)
 
 adjacency = pairs %>%
-  left_join(codes, by = c('code1' = 'code')) %>% rename(country1 = country) %>%
-  left_join(codes, by = c('code2' = 'code')) %>% rename(country2 = country) %>%
+  left_join(codes, by = c('code1' = 'code')) %>% rename(unit1 = country) %>%
+  left_join(codes, by = c('code2' = 'code')) %>% rename(unit2 = country) %>%
   # require land/river contiguity (not large water bodies)
   filter(contiguity_type == 1) %>%
   filter(between(year, 2011, 2015)) %>%
-  select(year, country1, country2)
+  select(year, unit1, unit2)
 
 # make sure we got all the units, except for the islands
 my_units = adjacency %$%
-  c(country1, country2) %>%
+  c(unit1, unit2) %>%
   unique()
 
 missing_units = setdiff(units, my_units)
 stopifnot(setequal(missing_units, c('Iceland', 'Malta')))
 
-write_tsv(adjacency, '../../europe_adjacency.tsv')
+adjacency %>%
+  filter(unit1 %in% units, unit2 %in% units) %>%
+  select(-year) %>%
+  distinct() %>%
+  write_tsv('../../europe_adjacency.tsv')
