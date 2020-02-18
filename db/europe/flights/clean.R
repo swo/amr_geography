2b1b1083a1dd0a7800de463b660a44ad9ca8e372
swo <swo@alum.mit.edu>
2020-02-17 21:59:10 -0500

    cleaner flights

--- db/europe/flights/clean.R
+++ db/europe/flights/clean.R
@@ -5,14 +5,7 @@ library(countrycode)
 library(magrittr)
 
 # Parameters
-year <- "2013"
-f_internal <- 0.01
-
-# Get the list of countries we care about
-countries <- read_tsv("../../../data/ecdc/data.tsv") %>%
-  pull(country) %>%
-  unique() %>%
-  countrycode("country.name", "iso.name.en")
+year <- "2018"
 
 # Read in flight data
 flights_raw <- read_tsv("avia_paocc.tsv.gz", na = ":")
@@ -24,19 +17,29 @@ flights <- flights_raw %>%
   # use only passengers carried
   filter(unit == "PAS", measure == "PAS_CRD") %>%
   # use ISO country names
-  mutate_at(c("partner", "geo"), ~ countrycode(., "eurostat", "iso.name.en")) %>%
+  mutate_at(c("partner", "geo"), ~ countrycode(., "eurostat", "iso3c")) %>%
+  filter_at(c("partner", "geo"), ~ !is.na(.)) %>%
   # keep only the 2 country names and the number of passenders in that year
-  select(country1 = partner, country2 = geo, !!c("n" = year)) %>%
-  filter(country1 %in% countries, country2 %in% countries)
+  select(country1 = partner, country2 = geo, !!c("n" = year))
 
 # load population
 population_raw <- read_tsv("demo_pjan.tsv.gz", na = ":")
 
+remove_annotations <- function(x) {
+  if (is.numeric(x)) {
+    x
+  } else if (is.character(x)) {
+    as.numeric(str_replace(x, " p$", ""))
+  }
+}
+
 population <- population_raw %>%
   separate(!!names(.)[1], c("unit", "age", "sex", "geo"), sep = ",") %>%
   filter(unit == "NR", age == "TOTAL", sex == "T") %>%
-  mutate_at("geo", ~ countrycode(., "eurostat", "iso.name.en")) %>%
-  select(country = geo, !!c("n_population" = year))
+  select(-unit, -age, -sex) %>%
+  mutate_at("geo", ~ countrycode(., "eurostat", "iso3c")) %>%
+  select(country = geo, !!c("n_population" = year)) %>%
+  mutate_at("n_population", remove_annotations)
 
 # get "normalized" internal flights
 normalization_data <- flights %>%
@@ -47,28 +50,12 @@ model <- lm(n ~ n_population + 0, data = normalization_data)
 
 # Add model predictions
 normalized_data <- normalization_data %>%
-  mutate(n = (1 / f_internal) * predict(model, newdata = select(., n_population))) %>%
+  mutate(n = floor(predict(model, newdata = select(., n_population)))) %>%
   select(country1, country2, n)
 
 new_data <- flights %>%
   filter(country1 != country2) %>%
   bind_rows(normalized_data) %>%
   arrange(country1, country2)
-
-# Take flights A->B and B->A, and average then for a single A-B value
-# new_data <- left_join(
-#   filter(flights, country1 < country2),
-#   filter(flights, country1 > country2),
-#   by = c("country1" = "country2", "country2" = "country1")
-# ) %>%
-#   mutate(n = 0.5 * (n.x + n.y)) %>%
-#   select(country1, country2, n) %>%
-#   # Add in the A-A values
-#   bind_rows(normalized_data) %>%
-#   replace_na(list(n = 0)) %>%
-#   arrange(country1, country2) %>%
-#   group_by(country1) %>%
-#   # Convert to fractions of contacts
-#   mutate(f = n / sum(n, na.rm = TRUE))
 
 write_tsv(new_data, "../flights.tsv")
