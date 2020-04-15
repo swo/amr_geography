d4a1bb1f03ff88145fba2be6fa6cfa79fa517455
swo <swo@alum.mit.edu>
2020-04-15 15:59:49 -0500

    melded utils

--- analysis/utils.R
+++ analysis/utils.R
@@ -82,3 +82,15 @@ movement_counts_to_transmission_matrix <- function(tbl, size = NULL, f_internal 
   scale_rows(out)
 }
 
+# Fig utils
+
+library(cowplot)
+library(grid)
+library(gridExtra)
+library(patchwork)
+
+pdf.options(useDingbats = FALSE, useKerning = FALSE)
+
+annotate_text <- function(x, y, label, parse = TRUE, ...) {
+  annotate('text', x = x, y = y, label = label, parse = parse, ...)
+}
