83ff17e33da227e0cf366ed5a492a9be2bd70c48
swo <swo@alum.mit.edu>
2020-05-30 10:58:04 -0500

    major reworking

--- analysis/empirical_analysis.R
+++ analysis/empirical_analysis.R
@@ -4,8 +4,10 @@ library(tidyverse)
 library(datasets)
 library(countrycode)
 library(cowplot)
+library(patchwork)
+library(scales)
 
-revalue <- function(x, to, from) to[match(x, from)]
+revalue <- function(x, from, to) to[match(x, from)]
 
 # Load US data --------------------------------------------------------
 
@@ -25,8 +27,8 @@ marketscan_use <- read_tsv('../data/ms-medicare-ro/ineq_marketscan.tsv') %>%
 
 marketscan <- marketscan_use %>%
   inner_join(marketscan_res, by = c('drug', 'state')) %>%
-  select(unit = state, bugdrug, use, f_resistant) %>%
-  mutate_at("unit", ~ revalue(., state.name, state.abb))
+  mutate_at("state", ~ revalue(., state.name, state.abb)) %>%
+  select(unit = state, bugdrug, use, f_resistant)
 
 nhsn <- read_tsv('../data/nhsn-ims/data.tsv') %>%
   filter(state %in% state.abb) %>%
@@ -66,63 +68,19 @@ eu_units <- unique(europe$unit)
 
 # Build combined use/resistance dataset ---------------------------------------
 
-unit_data <- bind_rows(
-  'MarketScan/RO' = marketscan,
-  'Xponent/NHSN' = nhsn,
-  'ECDC' = europe,
-  .id = 'data_source'
+unit_data <- tribble(
+  ~setting, ~data_source, ~data,
+  "US", "MarketScan/RO", marketscan,
+  "US", "Xponent/NHSN", nhsn,
+  "Europe", "ECDC", europe
 ) %>%
+  unnest(cols = data) %>%
   mutate(dataset = str_c(data_source, ' ', bugdrug)) %>%
-  select(dataset, unit, f_resistant, use) %>%
+  select(dataset, setting, unit, f_resistant, use) %>%
   group_by(dataset) %>%
   nest() %>%
   ungroup() %>%
   mutate_at('dataset', fct_inorder)
-
-# Load adjacency and commuting data -------------------------------------------
-
-matrixify <- function(df) {
-  stopifnot(all(names(df)[-1] == df$from_unit))
-  
-  mat <- df %>%
-    select(-from_unit) %>%
-    as.matrix %>%
-    `rownames<-`(names(df)[-1])
-  
-  stopifnot(all(rownames(mat) == colnames(mat)))
-  
-  # make rows sum to 1
-  mat <- mat %>%
-    sweep(1, rowSums(.), "/")
-  #stopifnot(isTRUE(all.equal(rowSums(mat), 1)))
-  
-  # symmetrize
-  #mat <- 0.5 * (mat + t(mat))
-  #stopifnot(all(mat == t(mat)))
-  
-  mat
-}
-
-interactions_db <- tribble(
-  ~setting, ~metric, ~fn,
-  "US", "adjacency", "../db/us/adjacency.tsv",
-  "US", "commuting", "../db/us/commuting.tsv",
-  "Europe", "adjacency", "../db/europe/adjacency.tsv",
-  "Europe", "commuting", "../db/europe/commuting.tsv"
-) %>%
-  mutate(
-    data = map(fn, read_tsv),
-    interactions = map(data, matrixify)
-  )
-
-
-# commuting_db <- bind_rows(
-#   'US' = us_commuting,
-#   'Europe' = eu_commuting,
-#   .id = 'setting'
-# ) %>%
-#   mutate_at('setting', fct_inorder)
-
 
 # Use-resistance in different datasets ----------------------------------------
 
@@ -158,6 +116,79 @@ obs_plot <- unit_data %>%
 
 ggsave('fig/cross_sectional.pdf')
 
+# Load adjacency and commuting data -------------------------------------------
+
+read_adjacency_matrix <- function(fn) {
+  read_tsv(fn) %>%
+    rename(unit1 = from_unit) %>%
+    pivot_longer(
+      ., -unit1,
+      names_to = "unit2", values_to = "adjacent"
+    )
+}
+
+matrixify <- function(df) {
+  stopifnot(all(names(df)[-1] == df$from_unit))
+  
+  mat <- df %>%
+    select(-from_unit) %>%
+    as.matrix %>%
+    `rownames<-`(names(df)[-1])
+  
+  stopifnot(all(rownames(mat) == colnames(mat)))
+  
+  # make rows sum to 1
+  mat <- mat %>%
+    sweep(1, rowSums(.), "/")
+    
+  stopifnot(all(rowSums(mat) == 1))
+  
+  # symmetrize
+  mat <- 0.5 * (mat + t(mat))
+  stopifnot(all(mat == t(mat)))
+    
+  # set diagonal to maximum
+  diag(mat) <- max(mat)
+  
+  mat
+}
+
+tibblify <- function(mat) {
+  as_tibble(mat, rownames = "unit1") %>%
+    pivot_longer(-unit1, names_to = "unit2", values_to = "interaction")
+}
+
+adjacency_db <- tribble(
+  ~setting, ~fn,
+  "US", "../db/us/adjacency.tsv",
+  "Europe", "../db/europe/adjacency.tsv"
+) %>%
+  mutate(data = map(fn, read_adjacency_matrix)) %>%
+  select(setting, data) %>%
+  unnest(cols = data) %>%
+  mutate_at("adjacent", as.logical)
+
+# check that European adjacency has all the names we expect and no more
+adjacency_db %>%
+  filter(setting == "Europe") %>%
+  { stopifnot(setequal(.$unit1, eu_units)) }
+
+interactions_matrices <- tribble(
+  ~setting, ~fn,
+  "US", "../db/us/commuting.tsv",
+  "Europe", "../db/europe/flights.tsv"
+) %>%
+  mutate(
+    data = map(fn, read_tsv),
+    matrix = map(data, matrixify)
+  ) %>%
+  select(setting, matrix)
+
+interactions_tbl <- interactions_matrices %>% 
+  mutate(tbl = map(matrix, tibblify)) %>%
+  select(setting, tbl) %>%
+  unnest(cols = tbl)
+
 # Pairs data ------------------------------------------------------------------
 
 cross_units <- function(df) {
@@ -166,14 +197,14 @@ cross_units <- function(df) {
     filter(unit1 < unit2) %>%
     left_join(rename_all(df, ~ str_c(., '1')), by = 'unit1') %>%
     left_join(rename_all(df, ~ str_c(., '2')), by = 'unit2') %>%
+    left_join(adjacency_db, by = c("unit1", "unit2")) %>%
+    left_join(interactions_tbl, by = c("unit1", "unit2")) %>%
     mutate(
       d_resistant = f_resistant1 - f_resistant2,
       d_use = use1 - use2,
       dr_du = d_resistant / d_use
     ) %>%
-    select(unit1, unit2, dr_du) #%>%
-    # left_join(adjacency_db, by = c('unit1', 'unit2')) %>%
-    # left_join(commuting_db, by = c('unit1', 'unit2'))
+    select(unit1, unit2, dr_du, adjacent, interaction)
 }
 
 leave_one_out_from_cross <- function(df) {
@@ -189,48 +220,36 @@ cross_data <- unit_data %>%
 
 # Commuting histogram ---------------------------------------------------------
 
-x <- bind_rows(
-  adjacency = us_adjacency,
-  commuting = us_commuting,
-  .id = "metric"
-) %>%
-  pivot_longer(cols = c(-metric, -from_unit), names_to = "to_unit") %>%
-  pivot_wider(c(metric, from_unit, to_unit), names_from = "metric") %>%
-  mutate_at("adjacency", as.logical)
-
-stop("OK")
-
-commuting_histogram <- cross_data %>%
-  select(cross_data) %>%
-  unnest(cols = c(cross_data)) %>%
-  select(setting, unit1, unit2, adjacent, f_commuting) %>%
-  filter(
-    unit1 < unit2,
-    !is.na(f_commuting)
-  ) %>%
-  ggplot(aes(log10(f_commuting), fill = adjacent)) +
-  facet_wrap(~ setting, scales = 'free') +
-  geom_histogram(color = 'black') +
+histograms <- cross_data %>%
+  select(setting, cross_data) %>%
+  unnest(cols = cross_data) %>%
+  select(setting, unit1, unit2, adjacent, interaction) %>%
+  distinct() %>%
+  filter(unit1 < unit2) %>%
+  mutate(key = recode(
+    setting,
+    Europe = "European intercountry flights",
+    US = "US interstate commuting"
+  )) %>%
+  ggplot(aes(x = interaction, fill = adjacent)) +
+  geom_histogram(na.rm = TRUE, color = "black") +
+  facet_wrap(facets = vars(key), scales = "free") +
+  scale_x_log10() +
+  scale_y_continuous(expand = c(0, 0)) +
   scale_fill_manual(
-    limits = c('TRUE', 'FALSE'),
-    labels = c('adjacent', 'not adj.'),
-    values = c('black', 'white')
+    breaks = c(FALSE, TRUE),
+    values = c("white", "black"),
+    labels = c("no", "yes")
   ) +
-  xlab('Commuting fraction (log 10)') +
-  ylab('Number of states or countries') +
-  theme_cowplot() +
-  theme(
-    strip.background = element_blank(),
-    legend.position = c(0.85, 0.75),
-    legend.title = element_blank()
-  )
+  labs(
+    x = "Ad hoc interaction",
+    y = "No. of unit pairs",
+    fill = "Adjacent?"
+  ) +
+  theme_cowplot()
 
 ggsave('fig/commuting_histogram.pdf')
 
-cat('Number of pairs with 0 commuting fraction\n')
-commuting_db %>%
-  filter(unit1 < unit2, !is.na(f_commuting)) %>%
-  count(dataset, f_commuting == 0)
 
 # Adjacency analysis ----------------------------------------------------------
 
@@ -254,7 +273,7 @@ analysis_f <- function(model_f, coef_f, ratio_f) {
 
   l1o_results <- cross_data %>%
     select(dataset, l1o_cross_data) %>%
-    unnest() %>%
+    unnest(cols = l1o_cross_data) %>%
     mutate(
       model = map(l1o_cross_data, model_f),
       coef = map_dbl(model, coef_f),
@@ -270,8 +289,6 @@ analysis_f <- function(model_f, coef_f, ratio_f) {
     left_join(l1o_results, by = 'dataset')
 }
 
-rlm <- MASS::rlm
-
 adjacency_results <- analysis_f(
   function(df) with(df, {
     list(
@@ -283,70 +300,53 @@ adjacency_results <- analysis_f(
   function(model) with(model, { (adj_med - nonadj_med) / nonadj_med })
 )
 
-adjacency_lorru_results <- analysis_f(
-  function(df) with(df, {
-    list(
-      adj_med = median(df$lorr_du[df$adjacent]),
-      nonadj_med = median(df$lorr_du[!df$adjacent])
-    )
-  }),
-  function(model) with(model, { adj_med - nonadj_med }),
-  function(model) with(model, { (adj_med - nonadj_med) / nonadj_med })
-)
 
-adjacency_rlm_results <- analysis_f(
-  function(df) rlm(dr_du ~ adjacent, data = df),
-  function(model) coef(model)['adjacentTRUE'],
-  function(model) coef(model) %>% { .['adjacentTRUE'] / .['(Intercept)'] }
-)
-
-adjacency_covariates_results <- analysis_f(
-  function(df) rlm(dr_du ~ adjacent + d_income + d_temperature + d_density, data = df),
-  function(model) coef(model)['adjacentTRUE'],
-  function(model) coef(model) %>% { .['adjacentTRUE'] / .['(Intercept)'] }
-)
+# Commuting analysis --------------------------------------------------
 
 commuting_results <- analysis_f(
-  function(df) rlm(dr_du ~ f_commuting, data = df),
-  function(model) coef(model)['f_commuting'] * 1e-4,
-  function(model) coef(model) %>% { .['f_commuting'] / .['(Intercept)'] * 1e-4 }
+  function(df) cor(df$dr_du, df$interaction, method = "spearman"),
+  function(model) model,
+  function(model) NA
 )
 
-# Tables ----------------------------------------------------------------------
+stop("run the permutation test")
 
-show_results <- function(df, caption) {
-  df %>%
-    mutate(
-      coef_hci = 1.96 * coef_se * 0.5,
-      coef_cil = coef - coef_hci,
-      coef_ciu = coef + coef_hci,
-      coef_star = coef_cil > 0 | coef_ciu < 0,
-      ratio_hci = 1.96 * ratio_se * 0.5,
-      ratio_cil = ratio - ratio_hci,
-      ratio_ciu = ratio + ratio_hci,
-      ratio_star = ratio_cil > 0 | ratio_ciu < 0
-    ) %>%
-    mutate_if(is.numeric, sigfig) %>%
-    mutate_at(vars(ends_with('star')), ~ recode(as.numeric(.), `1` = '*', `0` = '')) %>%
-    mutate(
-      coef_display = str_glue('{coef} ({coef_cil} to {coef_ciu}){coef_star}'),
-      ratio_display = str_glue('{ratio} ({ratio_cil} to {ratio_ciu}){ratio_star}')
-    ) %>%
-    select(dataset, coef_display, ratio_display)
-}
-
-bind_rows(
-  "adjacency" = adjacency_results,
-  "adjacency_lorru" = adjacency_lorru_results,
-  "adjacency_rlm" = adjacency_rlm_results,
-  "commuting" = commuting_results,
-  .id = "model_type"
-) %>%
-  nest(-model_type) %>%
-  mutate(results = map(data, show_results)) %>%
-  select(model_type, results) %>%
-  unnest() %>%
-  write_tsv("results/empirical.tsv")
+# 
+# # Tables ----------------------------------------------------------------------
+# 
+# show_results <- function(df, caption) {
+#   df %>%
+#     mutate(
+#       coef_hci = 1.96 * coef_se * 0.5,
+#       coef_cil = coef - coef_hci,
+#       coef_ciu = coef + coef_hci,
+#       coef_star = coef_cil > 0 | coef_ciu < 0,
+#       ratio_hci = 1.96 * ratio_se * 0.5,
+#       ratio_cil = ratio - ratio_hci,
+#       ratio_ciu = ratio + ratio_hci,
+#       ratio_star = ratio_cil > 0 | ratio_ciu < 0
+#     ) %>%
+#     mutate_if(is.numeric, sigfig) %>%
+#     mutate_at(vars(ends_with('star')), ~ recode(as.numeric(.), `1` = '*', `0` = '')) %>%
+#     mutate(
+#       coef_display = str_glue('{coef} ({coef_cil} to {coef_ciu}){coef_star}'),
+#       ratio_display = str_glue('{ratio} ({ratio_cil} to {ratio_ciu}){ratio_star}')
+#     ) %>%
+#     select(dataset, coef_display, ratio_display)
+# }
+# 
+# bind_rows(
+#   "adjacency" = adjacency_results,
+#   "adjacency_lorru" = adjacency_lorru_results,
+#   "adjacency_rlm" = adjacency_rlm_results,
+#   "commuting" = commuting_results,
+#   .id = "model_type"
+# ) %>%
+#   nest(-model_type) %>%
+#   mutate(results = map(data, show_results)) %>%
+#   select(model_type, results) %>%
+#   unnest() %>%
+#   write_tsv("results/empirical.tsv")
 
 # show_results(
 #   adjacency_results,
@@ -371,86 +371,86 @@ bind_rows(
 
 # Plots -----------------------------------------------------------------------
 
-boxplot_data_f <- function(df, ymin, ymax) {
-  df %>%
-    nest(-adjacent) %>%
-    mutate(
-      y = map(data, ~ .$dr_du),
-      box = map(y, ~ boxplot.stats(.)$stats),
-      boxplot_data = map(box, ~ tibble(
-        ymin = max(.[1], ymin),
-        lower = .[2],
-        middle = .[3],
-        upper = .[4],
-        ymax = min(.[5], ymax)
-      ))
-    ) %>%
-    select(adjacent, boxplot_data) %>%
-    unnest()
-}
-
-boxplot_f <- function(cross_data, f_to_keep) {
-  half_drop <- (1 - f_to_keep) / 2
-
-  plot_data <- cross_data %>%
-    mutate(
-      y = map(cross_data, ~ .$dr_du),
-      ymin = map_dbl(y, ~ quantile(., half_drop)),
-      ymax = map_dbl(y, ~ quantile(., 1 - half_drop)),
-      boxplot_data = pmap(list(cross_data, ymin, ymax), boxplot_data_f),
-      point_data = pmap(list(cross_data, ymin, ymax), ~ filter(..1, between(dr_du, ..2, ..3)))
-    )
-
-  point_data <- plot_data %>%
-    select(dataset, point_data) %>%
-    unnest()
-
-  boxplot_data <- plot_data %>%
-    select(dataset, boxplot_data) %>%
-    unnest()
-
-  plot <- ggplot(data = NULL, aes(x = factor(adjacent))) +
-    facet_wrap(~ dataset, scales = 'free_y') +
-    geom_boxplot(
-      data = boxplot_data,
-      aes(lower = lower, upper = upper, middle = middle, ymin = ymin, ymax = ymax),
-      stat = 'identity'
-    ) +
-    geom_jitter(data = point_data, aes(y = dr_du), size = 0.1, width = 0.2) +
-    scale_x_discrete(
-      '',
-      labels = c(`TRUE` = 'Adjacent', `FALSE` = 'Not adj.')
-    ) +
-    ylab(expression(paste('Use-resistance association ', (Delta * rho / Delta * tau)))) +
-    theme_cowplot() +
-    theme(strip.background = element_blank())
-
-  plot
-}
-
-adjacency_plot <- boxplot_f(cross_data, 0.90)
-ggsave('fig/adjacency_plot.pdf', plot = adjacency_plot)
-
-# swo: can include the rlm on the un-logged x values, as reported in the
-# data, but they look weird and curvy when log-ing the x-vals
-
-commute_plot <- cross_data %>%
-  select(dataset, cross_data) %>%
-  unnest() %>%
-  group_by(dataset) %>%
-  mutate(x = case_when(
-    f_commuting == 0 ~ -6,
-    TRUE ~ log10(f_commuting)
-  )) %>%
-  filter(between(ecdf(dr_du)(dr_du), 0.025, 0.975)) %>%
-  ungroup() %>%
-  ggplot(aes(x)) +
-  facet_wrap(~ dataset, scales = 'free') +
-  geom_point(aes(y = dr_du), shape = 1, size = 0.5) +
-  #geom_line(aes(y = y)) +
-  xlab('Commuting fraction (log10)') +
-  ylab(expression(paste('Use-resistance association ', (Delta * rho / Delta * tau)))) +
-  theme_cowplot() +
-  theme(strip.background = element_blank())
-
-ggsave('fig/commute_plot.pdf', plot = commute_plot)
+# boxplot_data_f <- function(df, ymin, ymax) {
+#   df %>%
+#     nest(-adjacent) %>%
+#     mutate(
+#       y = map(data, ~ .$dr_du),
+#       box = map(y, ~ boxplot.stats(.)$stats),
+#       boxplot_data = map(box, ~ tibble(
+#         ymin = max(.[1], ymin),
+#         lower = .[2],
+#         middle = .[3],
+#         upper = .[4],
+#         ymax = min(.[5], ymax)
+#       ))
+#     ) %>%
+#     select(adjacent, boxplot_data) %>%
+#     unnest()
+# }
+# 
+# boxplot_f <- function(cross_data, f_to_keep) {
+#   half_drop <- (1 - f_to_keep) / 2
+# 
+#   plot_data <- cross_data %>%
+#     mutate(
+#       y = map(cross_data, ~ .$dr_du),
+#       ymin = map_dbl(y, ~ quantile(., half_drop)),
+#       ymax = map_dbl(y, ~ quantile(., 1 - half_drop)),
+#       boxplot_data = pmap(list(cross_data, ymin, ymax), boxplot_data_f),
+#       point_data = pmap(list(cross_data, ymin, ymax), ~ filter(..1, between(dr_du, ..2, ..3)))
+#     )
+# 
+#   point_data <- plot_data %>%
+#     select(dataset, point_data) %>%
+#     unnest()
+# 
+#   boxplot_data <- plot_data %>%
+#     select(dataset, boxplot_data) %>%
+#     unnest()
+# 
+#   plot <- ggplot(data = NULL, aes(x = factor(adjacent))) +
+#     facet_wrap(~ dataset, scales = 'free_y') +
+#     geom_boxplot(
+#       data = boxplot_data,
+#       aes(lower = lower, upper = upper, middle = middle, ymin = ymin, ymax = ymax),
+#       stat = 'identity'
+#     ) +
+#     geom_jitter(data = point_data, aes(y = dr_du), size = 0.1, width = 0.2) +
+#     scale_x_discrete(
+#       '',
+#       labels = c(`TRUE` = 'Adjacent', `FALSE` = 'Not adj.')
+#     ) +
+#     ylab(expression(paste('Use-resistance association ', (Delta * rho / Delta * tau)))) +
+#     theme_cowplot() +
+#     theme(strip.background = element_blank())
+# 
+#   plot
+# }
+# 
+# adjacency_plot <- boxplot_f(cross_data, 0.90)
+# ggsave('fig/adjacency_plot.pdf', plot = adjacency_plot)
+# 
+# # swo: can include the rlm on the un-logged x values, as reported in the
+# # data, but they look weird and curvy when log-ing the x-vals
+# 
+# commute_plot <- cross_data %>%
+#   select(dataset, cross_data) %>%
+#   unnest() %>%
+#   group_by(dataset) %>%
+#   mutate(x = case_when(
+#     f_commuting == 0 ~ -6,
+#     TRUE ~ log10(f_commuting)
+#   )) %>%
+#   filter(between(ecdf(dr_du)(dr_du), 0.025, 0.975)) %>%
+#   ungroup() %>%
+#   ggplot(aes(x)) +
+#   facet_wrap(~ dataset, scales = 'free') +
+#   geom_point(aes(y = dr_du), shape = 1, size = 0.5) +
+#   #geom_line(aes(y = y)) +
+#   xlab('Commuting fraction (log10)') +
+#   ylab(expression(paste('Use-resistance association ', (Delta * rho / Delta * tau)))) +
+#   theme_cowplot() +
+#   theme(strip.background = element_blank())
+# 
+# ggsave('fig/commute_plot.pdf', plot = commute_plot)
